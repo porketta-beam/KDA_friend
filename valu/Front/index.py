712837b6b9c36350585364a7e7fb1f0448f9e67d
porketta <aaaa0430@naver.com>
2025-06-06 17:59:48 +0900

    Start React project

--- valu/Front/index.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-pg = st.navigation([st.Page("report_0app.py"), st.Page("search_0app.py")])
-pg.run()
--- valu/Front/search_0app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def main():
-    st.title("Search")
-
-if __name__ == "__main__":
-    main()
